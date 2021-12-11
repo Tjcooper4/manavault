779405583066668d93c51653f8e3dd9ef4c5bc2c
TJ Cooper <tjcooper4@gmail.com>
2021-12-11 16:09:59 -0500

    Basic Card Page

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require('express'); 
 const app = express();
 const port = 3000;
 const path = require('path');
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname+'/views/index.html'));
+    
   });
 
 app.listen(port, () => {
